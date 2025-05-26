b1e5fdf7aba70f67bc8190d8ebed482ad6a8fee4
Rafael Matias <rafamatias102@gmail.com>
2025-05-26 21:23:02 +0100

    style: adjust layout of registration page for improved visual consistency

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -6,7 +6,7 @@ import RegisterForm  from "@/components/register-form"
 export default function LoginPage() {
   return (
     <div className="grid min-h-svh lg:grid-cols-2">
-      <div className="flex flex-col gap-4 p-6 md:p-10">
+      <div className="flex flex-col gap-4 p-6 md:p-10 overflow-hidden">
         <div className="flex justify-center gap-2 md:justify-start">
           <a href="#" className="flex items-center gap-2 font-medium">
             <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
@@ -15,7 +15,7 @@ export default function LoginPage() {
             Acme Inc.
           </a>
         </div>
-        <div className="flex flex-1 items-center justify-center">
+        <div className="flex flex-1 items-center justify-center min-h-[600px]">
           <div className="w-full max-w-xs">
             <RegisterForm />
           </div>
